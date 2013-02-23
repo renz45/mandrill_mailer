72f2262dfc3ef225db3700bb2f05262a44462732
Jonathan ES Lin <ernsheong@gmail.com>
2013-02-23 22:08:35 +0800

    Implement merge_vars option for Mandrill as :recipient_vars
    Provide another argument like the following example:
    recipient_vars: [{'email@email.com' => {'USER_NAME' => 'some name', 'USER_INFO => 'some info'}}]

--- spec/template_mailer_spec.rb
+++ spec/template_mailer_spec.rb
@@ -55,6 +55,18 @@ describe MandrillMailer::TemplateMailer do
     end
   end
 
+  describe '#mandrill_rcpt_args' do
+    let(:rcpt) { 'email@email.com' }
+    let(:arg_name) { 'USER_NAME' }
+    let(:arg_value) { 'bob' }
+
+    subject { mailer.send(:mandrill_rcpt_args, [{rcpt => {arg_name => arg_value}}]) }
+
+    it 'should convert the args to the merge_vars format' do
+      should eq [{'rcpt' => rcpt, 'vars' => [{'name' => arg_name, 'content' => arg_value}]}]
+    end
+  end
+
   describe '#format_to_params' do
     let(:email) { 'bob@email.com' }
     let(:name) { 'bob' }
@@ -99,6 +111,8 @@ describe MandrillMailer::TemplateMailer do
     let(:from_name) { 'Example Name' }
     let(:var_name) { 'USER_NAME' }
     let(:var_content) { 'bobert' }
+    let(:var_rcpt_name) { 'USER_INFO' }
+    let(:var_rcpt_content) { 'boboblacksheep' }
     let(:to_email) { 'bob@email.com' }
     let(:to_name) { 'bob' }
 
@@ -110,6 +124,9 @@ describe MandrillMailer::TemplateMailer do
         vars: {
           var_name => var_content
         },
+        recipient_vars: [
+          { to_email => { var_rcpt_name => var_rcpt_content } }
+        ],
         template_content: {template_content_name => template_content_content},
         headers: {"Reply-To" => "support@email.com"},
         bcc: 'email@email.com',
@@ -150,6 +167,7 @@ describe MandrillMailer::TemplateMailer do
         "url_strip_qs" => true,
         "bcc_address" => args[:bcc],
         "global_merge_vars" => [{"name" => var_name, "content" => var_content}],
+        "merge_vars" => [{"rcpt" => to_email, "vars" => [{"name" => var_rcpt_name, "content" => var_rcpt_content}]}],
         "tags" => args[:tags],
         "google_analytics_domains" => args[:google_analytics_domains],
         "google_analytics_campaign" => args[:google_analytics_campaign]

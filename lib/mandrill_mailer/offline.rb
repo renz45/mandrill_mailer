1848766a30f1a431af553ff926a87e7a535e6bfa
Pat Allan <pat@freelancing-gods.com>
2013-11-28 14:22:37 +1100

    Offline mode, useful for testing, overrides standard deliver method.

---
+++ lib/mandrill_mailer/offline.rb
@@ -0,0 +1,36 @@
+# Offline modifications for using MandrillMailer without actually sending
+# emails through Mandrill's API. Particularly useful for acceptance tests.
+#
+# To use, just require this file (say, in your spec_helper.rb):
+#
+#   require 'mandrill_mailer/offline'
+#
+# And then if you wish you can look at the contents of
+# MandrillMailer.deliveries to see whether an email was queued up by your test:
+#
+#   email = MandrillMailer::deliveries.detect { |mail|
+#     mail.template_name == 'my-template' &&
+#     mail.message['to'].any? { |to| to['email'] == 'my@email.com' }
+#   }
+#   expect(email).to_not be_nil
+#
+require 'mandrill_mailer'
+
+module MandrillMailer
+  def self.deliveries
+    @deliveries ||= []
+  end
+
+  class TemplateMailer
+    def deliver
+      MandrillMailer.deliveries << MandrillMailer::Mock.new({
+        :template_name    => template_name,
+        :template_content => template_content,
+        :message          => message,
+        :async            => async,
+        :ip_pool          => ip_pool,
+        :send_at          => send_at
+      })
+    end
+  end
+end

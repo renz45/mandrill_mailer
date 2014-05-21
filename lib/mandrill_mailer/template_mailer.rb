62b0baeef2858f6142e05d08d47ce22990691558
Nathan Bertram <nbertram@gmail.com>
2014-05-20 17:02:43 -0700

    Add support for images array (to support inline attached image with cid)

--- lib/mandrill_mailer/template_mailer.rb
+++ lib/mandrill_mailer/template_mailer.rb
@@ -267,7 +267,8 @@ module MandrillMailer
         "google_analytics_domains" => args[:google_analytics_domains],
         "google_analytics_campaign" => args[:google_analytics_campaign],
         "metadata" => args[:metadata],
-        "attachments" => mandrill_attachment_args(args[:attachments])
+        "attachments" => mandrill_attachment_args(args[:attachments]),
+        "images" => mandrill_images_args(args[:images])
       }
 
       unless MandrillMailer.config.interceptor_params.nil?
@@ -309,6 +310,17 @@ module MandrillMailer
     protected
 
     def mandrill_attachment_args(args)
+      return unless args
+      args.map do |attachment|
+        attachment.symbolize_keys!
+        type = attachment[:mimetype]
+        name = attachment[:filename]
+        file = attachment[:file]
+        {"type" => type, "name" => name, "content" => Base64.encode64(file)}
+      end
+    end
+
+    def mandrill_images_args(args)
       return unless args
       args.map do |attachment|
         attachment.symbolize_keys!

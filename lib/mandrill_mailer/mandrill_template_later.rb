fe8cce251ca0bbbd6cf31b3a3613db8395c7c7fb
Ben <BenRuns@users.noreply.github.com>
2015-12-22 10:58:44 -0800

    Update mandrill_template_later.rb

--- lib/mandrill_mailer/mandrill_template_later.rb
+++ lib/mandrill_mailer/mandrill_template_later.rb
@@ -1,17 +1,17 @@
 require 'mandrill_mailer/template_mailer'
 module MandrillMailer
-	class MandrillTemplateJob < ActiveJob::Base
-	  queue_as { MandrillMailer.config.deliver_later_queue_name }
+  class MandrillTemplateJob < ActiveJob::Base
+    queue_as { MandrillMailer.config.deliver_later_queue_name }
 
-	  def perform(template_name, template_content, message, async, ip_pool, send_at)
-	  	mailer = MandrillMailer::TemplateMailer.new
-	  	mailer.template_name = template_name
-	  	mailer.template_content = template_content
-	    mailer.message = message
-	    mailer.async = async
-	    mailer.ip_pool = ip_pool 
-	    mailer.send_at = send_at
-	  	mailer.deliver_now
-	  end
-	end
+    def perform(template_name, template_content, message, async, ip_pool, send_at)
+      mailer = MandrillMailer::TemplateMailer.new
+      mailer.template_name = template_name
+      mailer.template_content = template_content
+      mailer.message = message
+      mailer.async = async
+      mailer.ip_pool = ip_pool 
+      mailer.send_at = send_at
+      mailer.deliver_now
+    end
+  end
 end

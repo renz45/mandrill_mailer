80bbae000434517029a896c03e90bce6df1b41b2
Eric Anderson <eric@pixelwareinc.com>
2016-02-24 15:04:10 -0500

    Instatiate the mailer class when sending later.
    Out of the box this code is essentially a no-op but if the subclass
    has overridden functionality in the base mailer this ensures that
    overwritten functionality happens.
    
    I use this primarily to override `deliver_now` so that I can extract
    the mandril response the the REST call and store the message id's
    locally. Example:
    
        # Override to record the message id with each lead so we know when we last
        # communicated with a given lead. This also works even if `deliver_later` is
        # called as `deliver_later` internally calls `deliver_now`.
        def deliver_now
          super.tap do |results|
            emails = results.collect {|r| r['email']}
            leads = Lead.where(email: emails).index_by &:email
            for result in results
              leads[result['email']].communications.create! message_id: result['_id']
            end
          end
        end

--- lib/mandrill_mailer/mandrill_message_later.rb
+++ lib/mandrill_mailer/mandrill_message_later.rb
@@ -2,12 +2,12 @@ require 'mandrill_mailer/message_mailer'
 module MandrillMailer
   class MandrillMessageJob < ActiveJob::Base
     queue_as { MandrillMailer.config.deliver_later_queue_name }
- 
-    def perform(message, async, ip_pool, send_at)
-      mailer = MandrillMailer::MessageMailer.new
+
+    def perform(mailer, message, async, ip_pool, send_at)
+      mailer = mailer.constantize.new
       mailer.message = message
       mailer.async = async
-      mailer.ip_pool = ip_pool 
+      mailer.ip_pool = ip_pool
       mailer.send_at = send_at
       mailer.deliver_now
     end

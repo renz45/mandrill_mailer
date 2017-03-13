5ed05805e37eb47d6a089d23306ff4aa04a50fa5
Eric Anderson <eric@pixelwareinc.com>
2017-03-13 13:44:40 -0400

    Enfoce use of inline adapter.
    The specs assume the inline adapter is being used. This was true prior
    to Rails 5 but the new default in Rails 5 is the async adapter.
    
    We could possibly update the failing tests to be compatible with the
    async adapter but I think the inline adapter is better suited for
    tests generally anyway as it makes the tests easier and the difference
    in parity is not generally that worriesome considering most people don't
    use either adapter for production (so neither adapter has parity).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'pry'
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
+ActiveJob::Base.queue_adapter = :inline
+
 RSpec.configure do |config|
   config.mock_with :rspec
 end
